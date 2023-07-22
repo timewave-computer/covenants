c6b12df4366158b5975348565a55638e5646e91e
Udit Vira <udit.vira@gmail.com>
2023-07-22 09:09:36 -0400

    add initial comments

--- contracts/ls/src/msg.rs
+++ contracts/ls/src/msg.rs
@@ -7,10 +7,22 @@ use crate::state::ContractState;
 
 #[cw_serde]
 pub struct InstantiateMsg {
+    /// Address for the clock. This contract verifies
+    /// that only the clock can execute Ticks
     pub clock_address: String,
+    /// IBC transfer channel on Stride for Neutron
+    /// This is used to IBC transfer stuatom on Stride 
+    /// to the LP contract
     pub stride_neutron_ibc_transfer_channel_id: String,
+    /// IBC connection ID on Neutron for Stride
+    /// We make an Interchain Account over this connection
     pub neutron_stride_ibc_connection_id: String,
+    /// Address for the covenant's LP contract.
+    /// We send the liquid staked amount to this address
     pub lp_address: String,
+    /// The liquid staked denom (e.g., stuatom). This is
+    /// required because we only allow transfers of this denom
+    /// out of the LSer
     pub ls_denom: String,
     pub ibc_fee: IbcFee,
     pub ica_timeout: Uint64,
