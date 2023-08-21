dfe1203a61398283a7c4251f69b9b011f926b90b
bekauz <bekauz@protonmail.com>
2023-08-21 20:49:31 +0200

    init src

---
+++ contracts/single-party-pol/src/lib.rs
@@ -0,0 +1,12 @@
+#![warn(clippy::unwrap_used, clippy::expect_used)]
+
+extern crate core;
+
+pub mod contract;
+pub mod error;
+pub mod msg;
+pub mod state;
+
+// #[allow(clippy::unwrap_used)]
+// #[cfg(test)]
+// mod suite_test;
