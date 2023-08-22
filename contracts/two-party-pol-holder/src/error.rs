9825a26843339ad949519f91779ed6be598dda32
bekauz <bekauz@protonmail.com>
2023-08-22 23:58:23 +0200

    wip: ragequit

--- contracts/two-party-pol-holder/src/error.rs
+++ contracts/two-party-pol-holder/src/error.rs
@@ -15,4 +15,19 @@ pub enum ContractError {
 
     #[error("shares of covenant parties must add up to 1.0")]
     InvolvedPartiesConfigError {},
+
+    #[error("ragequit is disabled")]
+    RagequitDisabled {},
+
+    #[error("only covenant parties can initiate ragequit")]
+    RagequitUnauthorized {},
+
+    #[error("ragequit attempt with lockup period passed")]
+    RagequitWithLockupPassed {},
+
+    #[error("ragequit already active")]
+    RagequitAlreadyActive {},
+
+    #[error("no lp tokens available")]
+    NoLpTokensAvailable {},
 }
