173f9bc85fa413ff67cd68416876325f46786e23
bekauz <bekauz@protonmail.com>
2023-08-22 21:39:57 +0200

    messages, state

---
+++ contracts/two-party-pol-holder/src/contract.rs
@@ -0,0 +1,38 @@
+use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
+
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cw2::set_contract_version;
+
+use crate::{msg::InstantiateMsg, state::{POOL_ADDRESS, NEXT_CONTRACT, CLOCK_ADDRESS, RAGEQUIT_CONFIG, LOCKUP_CONFIG, PARTIES_CONFIG}, error::ContractError};
+
+const CONTRACT_NAME: &str = "crates.io:covenant-two-party-pol-holder";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    deps.api.debug("WASMDEBUG: covenant-two-party-pol-holder instantiate");
+
+    let pool_addr = deps.api.addr_validate(&msg.pool_address)?;
+    let next_contract = deps.api.addr_validate(&msg.next_contract)?;
+    let clock_addr = deps.api.addr_validate(&msg.clock_address)?;
+
+    let parties_config = msg.parties_config.validate()?;
+    let lockup_config = msg.lockup_config.validate(env.block)?;
+
+    POOL_ADDRESS.save(deps.storage, &pool_addr)?;
+    NEXT_CONTRACT.save(deps.storage, &next_contract)?;
+    CLOCK_ADDRESS.save(deps.storage, &clock_addr)?;
+    LOCKUP_CONFIG.save(deps.storage, &lockup_config)?;
+    RAGEQUIT_CONFIG.save(deps.storage, &msg.ragequit_config)?;
+    PARTIES_CONFIG.save(deps.storage, &parties_config)?;
+
+    // TODO: response atributes
+    Ok(Response::default())
+}
--- contracts/two-party-pol-holder/src/msg.rs
+++ contracts/two-party-pol-holder/src/msg.rs
@@ -1,22 +1,28 @@
+use std::cmp::Ordering;
+
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr, Uint128, Decimal, Uint64};
+use cosmwasm_std::{Addr, Decimal, Timestamp, BlockInfo};
 use covenant_macros::{clocked, covenant_deposit_address, covenant_clock_address, covenant_next_contract};
 
+use crate::error::ContractError;
 
 #[cw_serde]
 pub struct InstantiateMsg {
     /// Address for the clock. This contract verifies
     /// that only the clock can execute Ticks
     pub clock_address: String,
+    /// address of the pool
+    pub pool_address: String,
+    /// address of the next contract to forward the funds to.
+    /// usually expected tobe the splitter.
+    pub next_contract: String,
     /// block height of covenant expiration. Position is exited 
     /// automatically upon reaching that height.
-    pub expiration_height: u64,
-    /// address of the next contract to forward the funds to (splitter).
-    pub next_contract: Addr,
-    /// optional ragequit penalty denominated in decimals
-    pub ragequit_penalty: Option<Decimal>,
+    pub lockup_config: LockupConfig,
+    /// configuration for ragequit
+    pub ragequit_config: RagequitConfig,
     /// parties engaged in the POL.
-    pub whitelist_parties: Vec<Party>,
+    pub parties_config: PartiesConfig,
 }
 
 #[clocked]
@@ -48,17 +54,105 @@ pub enum ContractState {
 pub enum QueryMsg {
     #[returns(ContractState)]
     ContractState {},
-    #[returns(Option<Decimal>)]
-    RagequitPenalty {},
-    #[returns(Uint64)]
-    ExpirationHeight {},
-    #[returns(Vec<Party>)]
-    WhitelistParties {},
+    #[returns(RagequitConfig)]
+    RagequitConfig {},
+    #[returns(LockupConfig)]
+    LockupConfig {},
+    #[returns(PartiesConfig)]
+    PartiesConfig {},
+}
+
+#[cw_serde]
+pub struct PartiesConfig {
+    pub party_a: Party,
+    pub party_b: Party,
+}
+
+impl PartiesConfig {
+    /// validates the decimal shares of parties involved
+    /// that must add up to 1.0
+    pub fn validate(self) -> Result<PartiesConfig, ContractError> {
+        if self.party_a.share + self.party_b.share == Decimal::one() {
+            Ok(self)
+        } else {
+            Err(ContractError::InvolvedPartiesConfigError {})
+        }
+    }
 }
 
 #[cw_serde]
 pub struct Party {
+    /// authorized address of the party
     pub addr: Addr,
-    pub share: Uint128,
+    /// decimal share of the LP position (e.g. 1/2)
+    pub share: Decimal,
+    /// denom provided by the party
     pub provided_denom: String,
+    /// whether party is actively providing liquidity
+    pub active_position: bool,
+}
+
+#[cw_serde]
+pub enum RagequitConfig {
+    /// ragequit is disabled
+    Disabled,
+    /// ragequit is enabled with `RagequitTerms`
+    Enabled(RagequitTerms),
+}
+
+#[cw_serde]
+pub struct RagequitTerms {
+    /// decimal based penalty to be applied on a party
+    /// for initiating ragequit. this fraction is then
+    /// added to the counterparty that did not initiate
+    /// the ragequit
+    pub penalty: Decimal,
+    /// bool flag to indicate whether ragequit had been
+    /// initiated
+    pub active: bool,
+}
+
+/// enum based configuration of the lockup period.
+#[cw_serde]
+pub enum LockupConfig {
+    /// no lockup configured
+    None,
+    /// block height based lockup config
+    Block(u64),
+    /// timestamp based lockup config
+    Time(Timestamp),
+}
+
+impl LockupConfig {
+    /// validates that the lockup config being stored is not already expired.
+    pub fn validate(self, block_info: BlockInfo) -> Result<LockupConfig, ContractError> {
+        match self {
+            LockupConfig::None => Ok(self),
+            LockupConfig::Block(h) => {
+                if h > block_info.height {
+                    Ok(self)
+                } else {
+                    Err(ContractError::LockupValidationError {})
+                }
+            },
+            LockupConfig::Time(t) => {
+                if t.cmp(&block_info.time) != Ordering::Less {
+                    Ok(self)
+                } else {
+                    Err(ContractError::LockupValidationError {})
+                }
+            },
+        }
+    }
+
+    /// compares current block info with the stored lockup config.
+    /// returns false if no lockup configuration is stored.
+    /// otherwise, returns true if the current block is past the stored info.
+    pub fn is_due(self, block_info: BlockInfo) -> bool {
+        match self {
+            LockupConfig::None => false, // or.. true?
+            LockupConfig::Block(b) => block_info.height >= b,
+            LockupConfig::Time(t) => t.nanos() < block_info.time.nanos(),
+        }
+    }
 }
