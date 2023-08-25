96d7a5d6a20b170802f812e6038c08bd1265a616
bekauz <bekauz@protonmail.com>
2023-08-25 13:28:07 +0200

    unit tests passing

--- contracts/swap-holder/src/suite_tests/mod.rs
+++ contracts/swap-holder/src/suite_tests/mod.rs
@@ -1,4 +1,6 @@
-use cosmwasm_std::Empty;
+use cosmwasm_schema::{QueryResponses, cw_serde};
+use cosmwasm_std::{Empty, Binary, StdResult, Env, Deps, to_binary};
+use covenant_macros::covenant_deposit_address;
 use cw_multi_test::{Contract, ContractWrapper};
 use neutron_sdk::bindings::{
     msg::{IbcFee, NeutronMsg},
@@ -16,4 +18,29 @@ pub fn swap_holder_contract() -> Box<dyn Contract<Empty>> {
         crate::contract::query,
     );
     Box::new(contract)
+}
+
+pub fn mock_deposit_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        crate::contract::execute,
+        crate::contract::instantiate,
+        query,
+    );
+    Box::new(contract)
+}
+
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+
+#[covenant_deposit_address]
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::DepositAddress {} => Ok(to_binary(&"native-splitter")?)
+    }
 }
