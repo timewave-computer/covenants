397168c85aa61487ebee4b0a9dd9a06e0a7b5d4e
bekauz <bekauz@protonmail.com>
2023-08-26 13:37:19 +0200

    clock variable naming

--- contracts/clock/src/state.rs
+++ contracts/clock/src/state.rs
@@ -4,5 +4,5 @@ use cw_storage_plus::Item;
 
 pub(crate) const QUEUE: FIFOQueue<Addr> = FIFOQueue::new("front", "back", "count");
 pub(crate) const PAUSED: Item<bool> = Item::new("paused");
-pub(crate) const TICK_MAX_GAS: Item<Uint64> = Item::new("tmg");
+pub(crate) const TICK_MAX_GAS_LIMIT: Item<Uint64> = Item::new("tmg");
 pub(crate) const WHITELIST: Item<Vec<Addr>> = Item::new("whitelist");
