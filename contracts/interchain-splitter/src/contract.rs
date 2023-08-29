7c95c7c40a17935f3fb3521718e9a2442a335a49
bekauz <bekauz@protonmail.com>
2023-08-29 14:51:45 +0200

    base distribution

--- contracts/interchain-splitter/src/contract.rs
+++ contracts/interchain-splitter/src/contract.rs
@@ -2,12 +2,13 @@
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     DepsMut, Env, MessageInfo,
-    Response,
+    Response, Order, CosmosMsg,
 };
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{InstantiateMsg, ExecuteMsg};
+use crate::state::SPLIT_CONFIG_MAP;
 
 const CONTRACT_NAME: &str = "crates.io:covenant-interchain-splitter";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -21,6 +22,12 @@ pub fn instantiate(
 ) -> Result<Response, ContractError> {
     deps.api.debug("WASMDEBUG: instantiate");
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    // we validate the splits and store them per-denom
+    for (denom, split) in msg.splits {
+        let validated_split = split.validate_to_split_config()?;
+        SPLIT_CONFIG_MAP.save(deps.storage, denom, &validated_split)?;
+    }
 
     Ok(Response::default()
         .add_attribute("method", "interchain_splitter_instantiate")
@@ -37,10 +44,26 @@ pub fn execute(
     deps.api
         .debug(format!("WASMDEBUG: execute: received msg: {msg:?}").as_str());
     match msg {
-        ExecuteMsg::Tick {} => try_tick(deps, env, info),
+        ExecuteMsg::Tick {} => try_distribute(deps, env, info),
     }
 }
 
-fn try_tick(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, ContractError> {
-    Ok(Response::default())
+pub fn try_distribute(deps: DepsMut, env: Env, info: MessageInfo) -> Result<Response, ContractError> {
+    // first we query the contract balances
+    let balances = deps.querier.query_all_balances(env.contract.address)?;
+    let mut distribution_messages: Vec<CosmosMsg> = vec![];
+    // then we iterate over our split config and try to match the entries to available balances
+    for entry in SPLIT_CONFIG_MAP.range(deps.storage, None, None, Order::Ascending) {
+        let (denom, config) = entry?;
+
+        // if we have the denom in our balances we construct the split messages
+        if let Some(coin) = balances.iter().find(|c| c.denom == denom) {
+            let mut transfer_messages = config.get_transfer_messages(coin.amount, coin.denom.to_string())?;
+            distribution_messages.append(&mut transfer_messages);
+        }
+    }
+    
+    Ok(Response::default()
+        .add_messages(distribution_messages)
+    )
 }
--- contracts/interchain-splitter/src/msg.rs
+++ contracts/interchain-splitter/src/msg.rs
@@ -1,13 +1,131 @@
 use cosmwasm_schema::cw_serde;
 use covenant_macros::clocked;
+use cosmwasm_std::{IbcTimeout, Uint128, CosmosMsg, BankMsg, IbcMsg, Coin};
 
+use crate::error::ContractError;
 
 #[cw_serde]
 pub struct InstantiateMsg {
+    pub splits: Vec<(String, SplitType)>,
 }
 
 
 #[clocked]
 #[cw_serde]
 pub enum ExecuteMsg {
+}
+
+// for every receiver we need a few things:
+#[cw_serde]
+pub struct InterchainReceiver {
+    // 1. remote chain channel id
+    pub channel_id: String,
+    // 2. receiver address
+    pub address: String,
+    // 3. timeout info
+    pub ibc_timeout: IbcTimeout,
+}
+
+#[cw_serde]
+pub struct NativeReceiver {
+    pub address: String,
+}
+
+#[cw_serde]
+pub enum ReceiverType {
+    Interchain(InterchainReceiver),
+    Native(NativeReceiver),
+}
+
+#[cw_serde]
+pub enum SplitType {
+    Custom(SplitConfig),
+    TimewaveSplit,
+}
+
+impl SplitType {
+    pub fn validate_to_split_config(self) -> Result<SplitConfig, ContractError> {
+        match self {
+            SplitType::Custom(c) => c.validate(),
+            SplitType::TimewaveSplit => {
+                Ok(SplitConfig {
+                    receivers: vec![(
+                        ReceiverType::Native(NativeReceiver { address: "todo".to_string() }),
+                        Uint128::new(100)
+                    )],
+                })
+            },
+        }
+    }
+}
+
+#[cw_serde]
+pub struct SplitConfig {
+    pub receivers: Vec<(ReceiverType, Uint128)>,
+}
+
+impl SplitConfig {
+    pub fn validate(self) -> Result<SplitConfig, ContractError> {
+        let total_share: Uint128 = self.receivers
+            .iter()
+            .map(|r| r.1)
+            .sum();
+
+        if total_share == Uint128::new(100) {
+            Ok(self)
+        } else {
+            Err(ContractError::SplitMisconfig {})
+        }
+    }
+
+    /*
+                RefundConfig::Native(addr) => CosmosMsg::Bank(BankMsg::Send {
+                to_address: addr.to_string(),
+                amount: vec![Coin {
+                    denom: self.provided_denom,
+                    amount,
+                }],
+            }),
+            RefundConfig::Ibc(r_c_i) => CosmosMsg::Ibc(IbcMsg::Transfer {
+                channel_id: r_c_i.channel_id,
+                to_address: self.addr.to_string(),
+                amount: Coin {
+                    denom: self.provided_denom,
+                    amount,
+                },
+                timeout: IbcTimeout::with_timestamp(
+                    block.time.plus_seconds(r_c_i.ibc_transfer_timeout.u64()),
+                ),
+            }),
+     */
+    pub fn get_transfer_messages(self, amount: Uint128, denom: String) -> Result<Vec<CosmosMsg>, ContractError> {
+        let mut msgs: Vec<CosmosMsg> = vec![];
+
+        for (receiver_type, share) in self.receivers {
+            let entitlement = amount.checked_multiply_ratio(
+                share,
+                Uint128::new(100),
+            ).map_err(|_| ContractError::SplitMisconfig {})?;
+    
+            let amount = Coin {
+                denom: denom.to_string(),
+                amount: entitlement,
+            };
+            let msg = match receiver_type {
+                ReceiverType::Interchain(receiver) => CosmosMsg::Ibc(IbcMsg::Transfer {
+                    channel_id: receiver.channel_id,
+                    to_address: receiver.address,
+                    amount,
+                    timeout: receiver.ibc_timeout,
+                }),
+                ReceiverType::Native(receiver) => CosmosMsg::Bank(BankMsg::Send {
+                    to_address: receiver.address,
+                    amount: vec![amount],
+                }),
+            };
+            msgs.push(msg);
+        }
+        
+        Ok(msgs)
+    }
 }
