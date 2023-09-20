11bfa3ae43098c25ef26d4ecb98df45ebcdba7e4
bekauz <bekauz@protonmail.com>
2023-09-20 18:41:47 +0200

    tests cleanup

--- contracts/two-party-pol-holder/src/msg.rs
+++ contracts/two-party-pol-holder/src/msg.rs
@@ -1,32 +1,18 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{Addr, Decimal, Attribute};
 use covenant_macros::{clocked, covenant_clock_address, covenant_next_contract};
-use covenant_utils::{PolCovenantTerms, CovenantPartiesConfig, LockupConfig};
-
-use crate::error::ContractError;
+use covenant_utils::LockupConfig;
 
 #[cw_serde]
 pub struct InstantiateMsg {
-    /// Address for the clock. This contract verifies
-    /// that only the clock can execute Ticks
     pub clock_address: String,
-    /// address of the pool
     pub pool_address: String,
-    /// address of the next contract to forward the funds to.
-    /// usually expected tobe the splitter.
     pub next_contract: String,
-    /// block height of covenant expiration. Position is exited 
-    /// automatically upon reaching that height.
     pub lockup_config: LockupConfig,
-    /// configuration for ragequit
     pub ragequit_config: RagequitConfig,
-    /// parties engaged in the POL.
-    pub parties_config: CovenantPartiesConfig,
-    /// deadline for both parties to deposit the funds
-    /// TODO: rename LockupConfig to something more generic
-    /// to represent block/time based expiration
     pub deposit_deadline: Option<LockupConfig>,
-    pub covenant_terms: PolCovenantTerms,
+    pub party_a_router: String,
+    pub party_b_router: String,
 }
 
 impl InstantiateMsg {
@@ -36,7 +22,7 @@ impl InstantiateMsg {
             Attribute::new("pool_address", self.pool_address),
             Attribute::new("next_contract", self.next_contract),
         ];
-        attrs.extend(self.parties_config.get_response_attributes());
+        // attrs.extend(self.parties_config.get_response_attributes());
         attrs.extend(self.ragequit_config.get_response_attributes());
         attrs.extend(self.lockup_config.get_response_attributes());
         attrs
@@ -61,12 +47,12 @@ pub enum ContractState {
     /// of this contract that indicates an active LP position.
     /// TODO: think about whether this is a fair assumption to make.
     Active,
-    /// one of the parties have initiated ragequit.
-    /// party with an active position is free to exit at any time.
-    Ragequit,
-    /// covenant has reached its expiration date.
-    Expired,
-    /// underlying funds have been withdrawn.
+    // /// one of the parties have initiated ragequit.
+    // /// party with an active position is free to exit at any time.
+    // Ragequit,
+    // /// covenant has reached its expiration date.
+    // Expired,
+    // /// underlying funds have been withdrawn.
     Complete,
 }
 
@@ -81,96 +67,102 @@ pub enum QueryMsg {
     RagequitConfig {},
     #[returns(LockupConfig)]
     LockupConfig {},
-    #[returns(PartiesConfig)]
-    PartiesConfig {},
+    #[returns(Addr)]
+    PoolAddress {},
+    #[returns(Addr)]
+    RouterPartyA {},
+    #[returns(Addr)]
+    RouterPartyB {},
+    #[returns(LockupConfig)]
+    DepositDeadline {},
 }
 
-#[cw_serde]
-pub struct PartiesConfig {
-    pub party_a: Party,
-    pub party_b: Party,
-}
+// #[cw_serde]
+// pub struct PartiesConfig {
+//     pub party_a: Party,
+//     pub party_b: Party,
+// }
 
 
-impl PartiesConfig {
-    /// validates the decimal shares of parties involved
-    /// that must add up to 1.0
-    pub fn validate_config(&self) -> Result<&PartiesConfig, ContractError> {
-        if self.party_a.share + self.party_b.share == Decimal::one() {
-            Ok(self)
-        } else {
-            Err(ContractError::InvolvedPartiesConfigError {})
-        }
-    }
+// impl PartiesConfig {
+//     /// validates the decimal shares of parties involved
+//     /// that must add up to 1.0
+//     pub fn validate_config(&self) -> Result<&PartiesConfig, ContractError> {
+//         if self.party_a.share + self.party_b.share == Decimal::one() {
+//             Ok(self)
+//         } else {
+//             Err(ContractError::InvolvedPartiesConfigError {})
+//         }
+//     }
 
-    /// validates the caller and returns an error if caller is unauthorized,
-    /// or the calling party if its authorized
-    pub fn validate_caller(&self, caller: Addr) -> Result<Party, ContractError> {
-        let a = self.clone().party_a;
-        let b = self.clone().party_b;
-        if a.addr == caller {
-            Ok(a)
-        } else if b.addr == caller {
-            Ok(b)
-        } else {
-            Err(ContractError::RagequitUnauthorized {})
-        }
-    }
+//     /// validates the caller and returns an error if caller is unauthorized,
+//     /// or the calling party if its authorized
+//     pub fn validate_caller(&self, caller: Addr) -> Result<Party, ContractError> {
+//         let a = self.clone().party_a;
+//         let b = self.clone().party_b;
+//         if a.addr == caller {
+//             Ok(a)
+//         } else if b.addr == caller {
+//             Ok(b)
+//         } else {
+//             Err(ContractError::RagequitUnauthorized {})
+//         }
+//     }
 
-    /// subtracts the ragequit penalty to the ragequitting party
-    /// and adds it to the other party
-    pub fn apply_ragequit_penalty(
-        mut self,
-        rq_party: Party,
-        penalty: Decimal
-    ) -> Result<PartiesConfig, ContractError> {
-        if rq_party.addr == self.party_a.addr {
-            self.party_a.share -= penalty;
-            self.party_b.share += penalty;
-        } else {
-            self.party_a.share += penalty;
-            self.party_b.share -= penalty;
-        }
-        Ok(self)
-    }
+//     /// subtracts the ragequit penalty to the ragequitting party
+//     /// and adds it to the other party
+//     pub fn apply_ragequit_penalty(
+//         mut self,
+//         rq_party: Party,
+//         penalty: Decimal
+//     ) -> Result<PartiesConfig, ContractError> {
+//         if rq_party.addr == self.party_a.addr {
+//             self.party_a.share -= penalty;
+//             self.party_b.share += penalty;
+//         } else {
+//             self.party_a.share += penalty;
+//             self.party_b.share -= penalty;
+//         }
+//         Ok(self)
+//     }
 
-    pub fn get_party_by_addr(self, addr: Addr) -> Result<Party, ContractError> {
-        if self.party_a.addr == addr {
-            Ok(self.party_a)
-        } else if self.party_b.addr == addr {
-            Ok(self.party_b)
-        } else {
-            Err(ContractError::PartyNotFound {})
-        }
-    }
-}
+//     pub fn get_party_by_addr(self, addr: Addr) -> Result<Party, ContractError> {
+//         if self.party_a.addr == addr {
+//             Ok(self.party_a)
+//         } else if self.party_b.addr == addr {
+//             Ok(self.party_b)
+//         } else {
+//             Err(ContractError::PartyNotFound {})
+//         }
+//     }
+// }
 
-impl PartiesConfig {
-    pub fn get_response_attributes(self) -> Vec<Attribute> {
-        vec![
-            Attribute::new("party_a_address", self.party_a.addr),
-            Attribute::new("party_a_share", self.party_a.share.to_string()),
-            Attribute::new("party_a_provided_denom", self.party_a.provided_denom),
-            Attribute::new("party_a_active_position", self.party_a.active_position.to_string()),
-            Attribute::new("party_b_address", self.party_b.addr),
-            Attribute::new("party_b_share", self.party_b.share.to_string()),
-            Attribute::new("party_b_provided_denom", self.party_b.provided_denom),
-            Attribute::new("party_b_active_position", self.party_b.active_position.to_string()),
-        ]
-    }
-}
+// impl PartiesConfig {
+//     pub fn get_response_attributes(self) -> Vec<Attribute> {
+//         vec![
+//             Attribute::new("party_a_address", self.party_a.addr),
+//             Attribute::new("party_a_share", self.party_a.share.to_string()),
+//             Attribute::new("party_a_provided_denom", self.party_a.provided_denom),
+//             Attribute::new("party_a_active_position", self.party_a.active_position.to_string()),
+//             Attribute::new("party_b_address", self.party_b.addr),
+//             Attribute::new("party_b_share", self.party_b.share.to_string()),
+//             Attribute::new("party_b_provided_denom", self.party_b.provided_denom),
+//             Attribute::new("party_b_active_position", self.party_b.active_position.to_string()),
+//         ]
+//     }
+// }
 
-#[cw_serde]
-pub struct Party {
-    /// authorized address of the party
-    pub addr: Addr,
-    /// decimal share of the LP position (e.g. 1/2)
-    pub share: Decimal,
-    /// denom provided by the party
-    pub provided_denom: String,
-    /// whether party is actively providing liquidity
-    pub active_position: bool,
-}
+// #[cw_serde]
+// pub struct Party {
+//     /// authorized address of the party
+//     pub addr: Addr,
+//     /// decimal share of the LP position (e.g. 1/2)
+//     pub share: Decimal,
+//     /// denom provided by the party
+//     pub provided_denom: String,
+//     /// whether party is actively providing liquidity
+//     pub active_position: bool,
+// }
 
 #[cw_serde]
 pub enum RagequitConfig {
--- contracts/two-party-pol-holder/src/suite_tests/suite.rs
+++ contracts/two-party-pol-holder/src/suite_tests/suite.rs
@@ -1,5 +1,6 @@
-use crate::msg::{ContractState, ExecuteMsg, InstantiateMsg, QueryMsg};
-use cosmwasm_std::{Addr, Coin, Uint128};
+use crate::msg::{ContractState, ExecuteMsg, InstantiateMsg, QueryMsg, RagequitConfig};
+use cosmos_sdk_proto::tendermint::types::Block;
+use cosmwasm_std::{Addr, Coin, Uint128, BlockInfo, Uint64, Timestamp};
 use covenant_utils::{
     CovenantPartiesConfig, CovenantParty, LockupConfig, PolCovenantTerms,
 };
@@ -15,18 +16,21 @@ pub const DENOM_B: &str = "denom_b";
 pub const PARTY_A_ADDR: &str = "party_a";
 pub const PARTY_B_ADDR: &str = "party_b";
 
+pub const PARTY_A_ROUTER: &str = "party_a_router";
+pub const PARTY_B_ROUTER: &str = "party_b_router";
+
 pub const CLOCK_ADDR: &str = "clock_address";
-pub const NEXT_CONTRACT: &str = "next_contract";
+pub const NEXT_CONTRACT: &str = "contract0";
 
 pub const INITIAL_BLOCK_HEIGHT: u64 = 12345;
 pub const INITIAL_BLOCK_NANOS: u64 = 1571797419879305533;
+
+pub const POOL: &str = "some_pool";
 
 pub struct Suite {
     pub app: App,
     pub holder: Addr,
     pub mock_deposit: Addr,
-    pub party_a: CovenantParty,
-    pub party_b: CovenantParty,
 }
 
 pub struct SuiteBuilder {
@@ -38,17 +42,14 @@ impl Default for SuiteBuilder {
     fn default() -> Self {
         Self {
             instantiate: InstantiateMsg {
-                pool_address: todo!(),
-                ragequit_config: todo!(),
+                pool_address: POOL.to_string(),
+                ragequit_config: RagequitConfig::Disabled,
                 deposit_deadline: None,
                 clock_address: CLOCK_ADDR.to_string(),
                 next_contract: NEXT_CONTRACT.to_string(),
                 lockup_config: LockupConfig::None,
-                parties_config: todo!(),
-                covenant_terms: PolCovenantTerms {
-                    party_a_amount: Uint128::new(400),
-                    party_b_amount: Uint128::new(20),
-                },
+                party_a_router: PARTY_A_ROUTER.to_string(),
+                party_b_router: PARTY_B_ROUTER.to_string(),
             },
             app: App::default(),
         }
@@ -58,6 +59,11 @@ impl Default for SuiteBuilder {
 impl SuiteBuilder {
     pub fn with_lockup_config(mut self, config: LockupConfig) -> Self {
         self.instantiate.lockup_config = config;
+        self
+    }
+
+    pub fn with_deposit_deadline(mut self, config: LockupConfig) -> Self {
+        self.instantiate.deposit_deadline = Some(config);
         self
     }
 
@@ -94,8 +100,6 @@ impl SuiteBuilder {
             app,
             holder,
             mock_deposit,
-            party_a: self.instantiate.parties_config.party_a,
-            party_b: self.instantiate.parties_config.party_b,
         }
     }
 }
@@ -118,6 +122,34 @@ impl Suite {
         self.app
             .wrap()
             .query_wasm_smart(&self.holder, &QueryMsg::NextContract {})
+            .unwrap()
+    }
+
+    pub fn query_pool(&self) -> Addr {
+        self.app
+            .wrap()
+            .query_wasm_smart(&self.holder, &QueryMsg::PoolAddress {})
+            .unwrap()
+    }
+
+    pub fn query_router_party_a(&self) -> Addr {
+        self.app
+            .wrap()
+            .query_wasm_smart(&self.holder, &QueryMsg::RouterPartyA {})
+            .unwrap()
+    }
+
+    pub fn query_router_party_b(&self) -> Addr {
+        self.app
+            .wrap()
+            .query_wasm_smart(&self.holder, &QueryMsg::RouterPartyB {})
+            .unwrap()
+    }
+
+    pub fn query_deposit_deadline(&self) -> LockupConfig {
+        self.app
+            .wrap()
+            .query_wasm_smart(&self.holder, &QueryMsg::DepositDeadline {})
             .unwrap()
     }
 
@@ -154,6 +186,10 @@ impl Suite {
             .update_block(|mut b| b.time = b.time.plus_minutes(n));
     }
 
+    pub fn get_current_block(&mut self) -> BlockInfo {
+        self.app.block_info()
+    }
+
     pub fn fund_coin(&mut self, coin: Coin) -> AppResponse {
         self.app
             .sudo(SudoMsg::Bank(cw_multi_test::BankSudo::Mint {
@@ -161,5 +197,37 @@ impl Suite {
                 amount: vec![coin],
             }))
             .unwrap()
+    }
+
+    pub fn get_denom_a_balance(&mut self, addr: String) -> Uint128 {
+        self.app.wrap().query_balance(addr, DENOM_A).unwrap().amount
+    }
+
+    pub fn get_denom_b_balance(&mut self, addr: String) -> Uint128 {
+        self.app.wrap().query_balance(addr, DENOM_B).unwrap().amount
+    }
+
+    pub fn get_party_a_coin(&mut self, amount: Uint128) -> Coin {
+        Coin {
+            denom: DENOM_A.to_string(),
+            amount,
+        }
+    }
+
+    pub fn get_party_b_coin(&mut self, amount: Uint128) -> Coin {
+        Coin {
+            denom: DENOM_B.to_string(),
+            amount,
+        }
+    }
+
+
+}
+
+pub fn get_default_block_info() -> BlockInfo {
+    BlockInfo {
+        height: 12345,
+        time: Timestamp::from_nanos(1571797419879305533),
+        chain_id: "cosmos-testnet-14002".to_string(), 
     }
 }
