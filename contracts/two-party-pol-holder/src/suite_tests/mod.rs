6a5a4d4160498584681fd19e832a0f8f232e2eab
bekauz <bekauz@protonmail.com>
2023-09-21 13:54:42 +0200

    holder expiration

--- contracts/two-party-pol-holder/src/suite_tests/mod.rs
+++ contracts/two-party-pol-holder/src/suite_tests/mod.rs
@@ -1,5 +1,6 @@
+use astroport::asset::PairInfo;
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{to_binary, Binary, Deps, Empty, Env, StdResult};
+use cosmwasm_std::{to_binary, Binary, Deps, Empty, Env, StdResult, Addr};
 use covenant_macros::covenant_deposit_address;
 use cw_multi_test::{Contract, ContractWrapper};
 
@@ -24,6 +25,7 @@ pub fn mock_deposit_contract() -> Box<dyn Contract<Empty>> {
     Box::new(contract)
 }
 
+
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 
@@ -36,5 +38,29 @@ pub enum QueryMsg {}
 pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::DepositAddress {} => Ok(to_binary(&"splitter")?),
+    }
+}
+
+
+pub fn mock_astro_pool_contract() -> Box<dyn Contract<Empty>> {
+    let contract = ContractWrapper::new(
+        crate::contract::execute,
+        crate::contract::instantiate,
+        query_astro_pool,
+    );
+    Box::new(contract)
+}
+
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query_astro_pool(_deps: Deps, _env: Env, msg: astroport::pair::QueryMsg) -> StdResult<Binary> {
+    match msg {
+        astroport::pair::QueryMsg::Pair {} => Ok(to_binary(&PairInfo {
+            asset_infos: vec![],
+            contract_addr: Addr::unchecked("lp-token"),
+            liquidity_token: Addr::unchecked("lp-token"),
+            pair_type: astroport::factory::PairType::Xyk {  },
+        })?),
+        _ => Ok(to_binary(&"-")?),
     }
 }
