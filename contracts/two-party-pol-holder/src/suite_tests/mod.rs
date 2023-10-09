da8aea4be170afbeb32692e71dfd969db837068c
bekauz <bekauz@protonmail.com>
2023-10-09 16:32:16 +0200

    fmt; lints

--- contracts/two-party-pol-holder/src/suite_tests/mod.rs
+++ contracts/two-party-pol-holder/src/suite_tests/mod.rs
@@ -1,8 +1,11 @@
-use astroport::asset::{PairInfo, Asset};
+use astroport::asset::{Asset, PairInfo};
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{to_binary, Binary, Deps, Empty, Env, StdResult, Addr, Uint128, DepsMut, MessageInfo, Response, BankMsg, Coin};
+use cosmwasm_std::{
+    to_binary, Addr, BankMsg, Binary, Coin, Deps, DepsMut, Empty, Env, MessageInfo, Response,
+    StdResult, Uint128,
+};
 use covenant_macros::covenant_deposit_address;
-use cw20::{Cw20QueryMsg, BalanceResponse, Cw20ExecuteMsg};
+use cw20::{BalanceResponse, Cw20ExecuteMsg, Cw20QueryMsg};
 use cw_multi_test::{Contract, ContractWrapper};
 
 mod suite;
@@ -26,7 +29,6 @@ pub fn mock_deposit_contract() -> Box<dyn Contract<Empty>> {
     Box::new(contract)
 }
 
-
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 
@@ -45,7 +47,6 @@ pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
         QueryMsg::DepositAddress {} => Ok(to_binary(&"splitter")?),
     }
 }
-
 
 pub fn mock_astro_pool_contract() -> Box<dyn Contract<Empty>> {
     let contract = ContractWrapper::new(
@@ -67,34 +68,44 @@ pub fn mock_astro_lp_token_contract() -> Box<dyn Contract<Empty>> {
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute_lp_token(
-    deps: DepsMut,
-    env: Env,
+    _deps: DepsMut,
+    _env: Env,
     info: MessageInfo,
-    msg: Cw20ExecuteMsg,
+    _msg: Cw20ExecuteMsg,
 ) -> Result<Response, ContractError> {
     let msg = BankMsg::Send {
         to_address: info.sender.to_string(),
-        amount: vec![
-            Coin::new(200, DENOM_A),
-            Coin::new(200, DENOM_B),
-        ],
+        amount: vec![Coin::new(200, DENOM_A), Coin::new(200, DENOM_B)],
     };
     Ok(Response::default().add_message(msg))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query_astro_pool(_deps: Deps, _env: Env, msg: astroport::pair::QueryMsg) -> StdResult<Binary> {
+pub fn query_astro_pool(
+    _deps: Deps,
+    _env: Env,
+    msg: astroport::pair::QueryMsg,
+) -> StdResult<Binary> {
     match msg {
         astroport::pair::QueryMsg::Pair {} => Ok(to_binary(&PairInfo {
             asset_infos: vec![],
             contract_addr: Addr::unchecked("contract0"),
             liquidity_token: Addr::unchecked("contract0"),
-            pair_type: astroport::factory::PairType::Xyk {  },
+            pair_type: astroport::factory::PairType::Xyk {},
         })?),
-        astroport::pair::QueryMsg::Share { amount } => Ok(to_binary(&vec![
-            Asset { info: astroport::asset::AssetInfo::NativeToken { denom: DENOM_A.to_string() }, amount: Uint128::new(200) },
-            Asset { info: astroport::asset::AssetInfo::NativeToken { denom: DENOM_B.to_string() }, amount: Uint128::new(200) },
-
+        astroport::pair::QueryMsg::Share { amount: _ } => Ok(to_binary(&vec![
+            Asset {
+                info: astroport::asset::AssetInfo::NativeToken {
+                    denom: DENOM_A.to_string(),
+                },
+                amount: Uint128::new(200),
+            },
+            Asset {
+                info: astroport::asset::AssetInfo::NativeToken {
+                    denom: DENOM_B.to_string(),
+                },
+                amount: Uint128::new(200),
+            },
         ])?),
         _ => Ok(to_binary(&"-")?),
     }
@@ -103,8 +114,8 @@ pub fn query_astro_pool(_deps: Deps, _env: Env, msg: astroport::pair::QueryMsg) 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query_astro_lp_token(_deps: Deps, _env: Env, msg: cw20::Cw20QueryMsg) -> StdResult<Binary> {
     match msg {
-        Cw20QueryMsg::Balance { address } => Ok(to_binary(&BalanceResponse {
-            balance: Uint128::new(100)
+        Cw20QueryMsg::Balance { address: _ } => Ok(to_binary(&BalanceResponse {
+            balance: Uint128::new(100),
         })?),
         _ => Ok(to_binary(&"-")?),
     }
