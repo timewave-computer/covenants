f51a78535bf262109e74a44d3b517b2584dfb488
bekauz <bekauz@protonmail.com>
2023-10-12 17:46:25 +0200

    instantiation chain

--- contracts/two-party-pol-covenant/src/state.rs
+++ contracts/two-party-pol-covenant/src/state.rs
@@ -22,3 +22,5 @@ pub const COVENANT_CLOCK_ADDR: Item<Addr> = Item::new("covenant_clock_addr");
 pub const COVENANT_POL_HOLDER_ADDR: Item<Addr> = Item::new("covenant_two_party_pol_holder_addr");
 pub const PARTY_A_IBC_FORWARDER_ADDR: Item<Addr> = Item::new("party_a_ibc_forwarder_addr");
 pub const PARTY_B_IBC_FORWARDER_ADDR: Item<Addr> = Item::new("party_b_ibc_forwarder_addr");
+pub const PARTY_A_ROUTER_ADDR: Item<Addr> = Item::new("party_a_router_addr");
+pub const PARTY_B_ROUTER_ADDR: Item<Addr> = Item::new("party_b_router_addr");
