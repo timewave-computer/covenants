94a0f68b47caa63373b175c635621333f9a3de76
bekauz <bekauz@protonmail.com>
2023-12-23 19:45:57 +0100

    swap covenant native routing & ictests

--- contracts/swap-covenant/src/state.rs
+++ contracts/swap-covenant/src/state.rs
@@ -27,5 +27,5 @@ pub const COVENANT_INTERCHAIN_SPLITTER_ADDR: Item<Addr> =
 pub const COVENANT_SWAP_HOLDER_ADDR: Item<Addr> = Item::new("covenant_swap_holder_addr");
 pub const PARTY_A_IBC_FORWARDER_ADDR: Item<Addr> = Item::new("party_a_ibc_forwarder_addr");
 pub const PARTY_B_IBC_FORWARDER_ADDR: Item<Addr> = Item::new("party_b_ibc_forwarder_addr");
-pub const PARTY_A_INTERCHAIN_ROUTER_ADDR: Item<Addr> = Item::new("party_a_interchain_router_addr");
-pub const PARTY_B_INTERCHAIN_ROUTER_ADDR: Item<Addr> = Item::new("party_b_interchain_router_addr");
+pub const PARTY_A_ROUTER_ADDR: Item<Addr> = Item::new("party_a_router_addr");
+pub const PARTY_B_ROUTER_ADDR: Item<Addr> = Item::new("party_b_router_addr");
