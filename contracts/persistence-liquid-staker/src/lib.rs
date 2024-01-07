b6c9ae88337ac6a894a0b996ea9b0afe0510ace3
bekauz <bekauz@protonmail.com>
2024-01-07 23:29:05 +0100

    persistence liquid staker (preview)

---
+++ contracts/persistence-liquid-staker/src/lib.rs
@@ -0,0 +1,13 @@
+#![warn(clippy::unwrap_used, clippy::expect_used)]
+
+extern crate core;
+
+pub mod contract;
+pub mod error;
+pub mod helpers;
+pub mod msg;
+pub mod state;
+
+#[allow(clippy::unwrap_used)]
+#[cfg(test)]
+mod suite_test;
