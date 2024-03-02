52fe8234e33a835a04c1a2491b19420de1028690
bekauz <bekauz@protonmail.com>
2024-03-02 18:14:54 +0100

    swap holder unit tests suite setup

--- unit-tests/src/lib.rs
+++ unit-tests/src/lib.rs
@@ -13,5 +13,7 @@ pub mod test_two_party_pol_holder;
 #[cfg(test)]
 pub mod test_astroport_liquid_pooler;
 #[cfg(test)]
-pub mod single_party_holder;
+pub mod test_single_party_holder;
+#[cfg(test)]
+pub mod test_swap_holder;
 
