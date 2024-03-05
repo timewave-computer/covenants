86cd03196059a779e407df0c75b6e5035cd22462
bekauz <bekauz@protonmail.com>
2024-03-05 14:02:24 +0100

    adjust ibc forwarder test suite setup

--- unit-tests/src/test_ibc_forwarder/tests.rs
+++ unit-tests/src/test_ibc_forwarder/tests.rs
@@ -1,6 +1,10 @@
-use super::suite::Suite;
+use super::suite::IbcForwarderBuilder;
 
 #[test]
 fn test_covenant() {
-    let mut suite = Suite::new_default();
+    let suite = IbcForwarderBuilder::default()
+        .with_remote_chain_connection_id("some other connection".to_string())
+        .build();
+
+    println!("suite rc info: {:?}", suite.remote_chain_info);
 }
