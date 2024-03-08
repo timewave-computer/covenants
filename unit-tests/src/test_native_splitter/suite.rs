03329dd03c99d44c4149799b7b34b59d4347b291
bekauz <bekauz@protonmail.com>
2024-03-08 17:06:51 +0100

    native splitter unit tests

--- unit-tests/src/test_native_splitter/suite.rs
+++ unit-tests/src/test_native_splitter/suite.rs
@@ -2,9 +2,10 @@ use std::{collections::BTreeMap, str::FromStr};
 
 use cosmwasm_std::{Addr, Decimal};
 use covenant_utils::split::SplitConfig;
+use cw_multi_test::{AppResponse, Executor};
 
 use crate::setup::{
-    base_suite::BaseSuiteMut, instantiates::native_splitter::NativeSplitterInstantiate, suite_builder::SuiteBuilder, CustomApp, CLOCK_SALT, DENOM_ATOM_ON_NTRN, DENOM_LS_ATOM_ON_NTRN, NATIVE_SPLITTER_SALT
+    base_suite::{BaseSuite, BaseSuiteMut}, instantiates::native_splitter::NativeSplitterInstantiate, suite_builder::SuiteBuilder, CustomApp, CLOCK_SALT, DENOM_ATOM_ON_NTRN, DENOM_LS_ATOM_ON_NTRN, NATIVE_SPLITTER_SALT
 };
 
 pub struct NativeSplitterBuilder {
@@ -19,7 +20,8 @@ impl Default for NativeSplitterBuilder {
         let clock_addr = builder.get_contract_addr(builder.clock_code_id, CLOCK_SALT);
         let native_splitter_addr =
             builder.get_contract_addr(builder.native_splitter_code_id, NATIVE_SPLITTER_SALT);
-
+        println!("code {:?}", builder.native_splitter_code_id);
+        
         let clock_instantiate_msg = covenant_clock::msg::InstantiateMsg {
             tick_max_gas: None,
             whitelist: vec![native_splitter_addr.to_string()],
@@ -89,7 +91,9 @@ impl NativeSplitterBuilder {
                 &covenant_native_splitter::msg::QueryMsg::Splits {},
             )
             .unwrap();
-
+        let config_1 = splits[0].clone().1.receivers;
+        let receivers: Vec<String> = config_1.keys().cloned().collect();
+        
         let split_map = BTreeMap::from_iter(splits);
 
         let fallback_split = self.builder
@@ -105,9 +109,12 @@ impl NativeSplitterBuilder {
             faucet: self.builder.faucet.clone(),
             admin: self.builder.admin.clone(),
             clock_addr,
+            splitter: native_splitter_address,
             splits: split_map,
             fallback_split,
             app: self.builder.build(),
+            receiver_1: Addr::unchecked(receivers[0].to_string()),
+            receiver_2: Addr::unchecked(receivers[1].to_string()),
         }
     }
 }
@@ -119,9 +126,76 @@ pub(super) struct Suite {
     pub faucet: Addr,
     pub admin: Addr,
 
+    pub splitter: Addr,
     pub clock_addr: Addr,
     pub splits: BTreeMap<String, SplitConfig>,
     pub fallback_split: Option<SplitConfig>,
+    pub receiver_1: Addr,
+    pub receiver_2: Addr,
+}
+
+impl Suite {
+    pub fn query_clock_address(&mut self) -> Addr {
+        self.app
+            .wrap()
+            .query_wasm_smart(
+                self.splitter.clone(),
+                &covenant_native_splitter::msg::QueryMsg::ClockAddress {},
+            )
+            .unwrap()
+    }
+
+    pub fn query_denom_split(&mut self, denom: String) -> SplitConfig {
+        self.app
+            .wrap()
+            .query_wasm_smart(
+                self.splitter.clone(),
+                &covenant_native_splitter::msg::QueryMsg::DenomSplit { denom },
+            )
+            .unwrap()
+    }
+
+    pub fn query_all_splits(&mut self) -> BTreeMap<String, SplitConfig> {
+        let splits: Vec<(String, SplitConfig)> = self.app
+            .wrap()
+            .query_wasm_smart(
+                self.splitter.clone(),
+                &covenant_native_splitter::msg::QueryMsg::Splits {},
+            )
+            .unwrap();
+        BTreeMap::from_iter(splits)
+    }
+
+    pub fn query_fallback_split(&mut self) -> Option<SplitConfig> {
+        self.app
+            .wrap()
+            .query_wasm_smart(
+                self.splitter.clone(),
+                &covenant_native_splitter::msg::QueryMsg::FallbackSplit {},
+            )
+            .unwrap()
+    }
+
+    pub fn query_deposit_address (&mut self) -> Addr {
+        self.app
+            .wrap()
+            .query_wasm_smart(
+                self.splitter.clone(),
+                &covenant_native_splitter::msg::QueryMsg::DepositAddress {},
+            )
+            .unwrap()
+    }
+
+    pub fn distribute_fallback(&mut self, denoms: Vec<String>) -> AppResponse {
+        self.app
+            .execute_contract(
+                self.faucet.clone(),
+                self.splitter.clone(),
+                &covenant_native_splitter::msg::ExecuteMsg::DistributeFallback { denoms },
+                &[]
+            )
+            .unwrap()
+    }
 }
 
 impl BaseSuiteMut for Suite {
@@ -135,5 +209,11 @@ impl BaseSuiteMut for Suite {
 
     fn get_faucet_addr(&mut self) -> Addr {
         self.faucet.clone()
+    }
+}
+
+impl BaseSuite for Suite {
+    fn get_app(&self) -> &CustomApp {
+        &self.app
     }
 }
