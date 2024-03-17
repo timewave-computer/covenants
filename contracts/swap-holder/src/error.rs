e9f41e500e05f528b9009e033e55ed64f11a17f2
bekauz <bekauz@protonmail.com>
2024-03-17 16:05:31 +0100

    using verify_clock to validate clock address in swap holder

--- contracts/swap-holder/src/error.rs
+++ contracts/swap-holder/src/error.rs
@@ -1,10 +1,14 @@
 use cosmwasm_std::StdError;
+use neutron_sdk::NeutronError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error(transparent)]
+    NeutronError(#[from] NeutronError),
 
     #[error("Unauthorized")]
     Unauthorized {},
