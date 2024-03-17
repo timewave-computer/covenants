d67cafeba7aa5a2e62b0bb0d653aa1047546bea1
bekauz <bekauz@protonmail.com>
2024-03-17 21:35:55 +0100

    removing todo!() macros

--- contracts/remote-chain-splitter/src/contract.rs
+++ contracts/remote-chain-splitter/src/contract.rs
@@ -382,7 +382,12 @@ pub fn migrate(deps: ExecuteDeps, _env: Env, msg: MigrateMsg) -> StdResult<Respo
 
             Ok(resp)
         }
-        MigrateMsg::UpdateCodeId { data: _ } => todo!(),
+        MigrateMsg::UpdateCodeId { data: _ } => {
+            // This is a migrate message to update code id,
+            // Data is optional base64 that we can parse to any data we would like in the future
+            // let data: SomeStruct = from_binary(&data)?;
+            Ok(Response::default())
+        }
     }
 }
 
