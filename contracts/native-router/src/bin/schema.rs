4a59de6d17196f859ef1ae3feaa5aa76e05a2295
bekauz <bekauz@protonmail.com>
2024-04-02 21:12:12 +0200

    schemas

---
+++ contracts/native-router/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+use covenant_native_router::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+        migrate: MigrateMsg,
+    }
+}
