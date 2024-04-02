71bce946bad8b1b7dcb9f01f691e0925b0bd2f71
bekauz <bekauz@protonmail.com>
2024-04-02 21:12:12 +0200

    schemas

---
+++ contracts/single-party-pol-holder/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+use covenant_single_party_pol_holder::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+        migrate: MigrateMsg,
+    }
+}
