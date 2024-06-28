d6dba4829241e66e4047513ba981d8486bf49b43
bekauz <bekauz@protonmail.com>
2024-06-28 15:05:57 +0200

    set optimizer image to optimizer; adjust e2e swap covenant tests

--- optimize.sh
+++ optimize.sh
@@ -3,12 +3,12 @@ if [[ $(uname -m) =~ "arm64" ]]; then \
     docker run --rm -v "$(pwd)":/code \
         --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
         --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-        cosmwasm/workspace-optimizer-arm64:0.14.0
+        cosmwasm/optimizer-arm64:0.16.0
 
 else
     docker run --rm -v "$(pwd)":/code \
         --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
         --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
         --platform linux/amd64 \
-        cosmwasm/workspace-optimizer:0.14.0
+        cosmwasm/optimizer:0.16.0
 fi
